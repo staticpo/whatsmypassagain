90eede8097044ee44f7a820b299a895cb613d324
staticpo <staticpo>
2020-01-02 21:24:44 -0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import QrReader from 'react-qr-reader'
 
 function App() {
+  const [read, setRead] = useState(0);
+
+  useEffect(() => {
+    document.title = `You scanned ${read}`;
+  });
+
+  const handleScan = (data) => {
+    setRead(data);
+  }
+
+  const handleError = err => {
+    console.error(err)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          --- {read} ---
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <QrReader
+          delay={300}
+          onError={handleError}
+          onScan={handleScan}
+          style={{ width: '100%' }}
+        />
       </header>
     </div>
   );
